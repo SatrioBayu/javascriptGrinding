c1d71dc69b349d53611895c3a0dcf3b3206d47c5
SatrioBayu <79488377+SatrioBayu@users.noreply.github.com>
2022-03-03 15:06:14 +0700

    edit comment
    nothing major

--- Playing with digits.js
+++ Playing with digits.js
@@ -1,6 +1,5 @@
-/* 
 
-Src: Codewars (https://www.codewars.com/kata/5552101f47fc5178b1000050/javascript)
+// Src: Codewars (https://www.codewars.com/kata/5552101f47fc5178b1000050/javascript)
 
 // My Code
 function digPow(n, p){
