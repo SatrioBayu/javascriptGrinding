939fdb0612a47a460d310c791bdd5fe3749a7acc
SatrioBayu <79488377+SatrioBayu@users.noreply.github.com>
2022-03-08 13:44:45 +0700

    Make it compact

--- Hashtag_Generator.js
+++ Hashtag_Generator.js
@@ -3,20 +3,7 @@
 // My First Code
 function generateHashtag (str) {
   let emptyRegex = /\w.*/g; // Check if the string is empty. "   " is also considered empty
-  let deleteSpaceRegex = /\w+/g // Select word without space
-  if(emptyRegex.test(str)==false){
-    return false // If the string is empty or not
-  } else{
-    let arrString = ['#',...str.match(deleteSpaceRegex)]; // Add '#' to first array and add array string using spread operator after get check with str.match to delete the space
-    for(let i=0; i<arrString.length;i++){
-      let temp = arrString[i][0].toUpperCase()+arrString[i].slice(1) // Make to first character in every word (array) to uppercase using temporary variable
-      arrString[i] = temp;
-    }
-    arrString = arrString.join(''); // Join the array to string
-    if(arrString.length-1<140){ // Check if the string length-1 < 140 or not. It's -1 because '#' doesn't count
-      return arrString;
-    } else{
-      return false;
-    }
-  }
+  return emptyRegex.test(str)==false? false : str.split(' ').join('').length < 140
+  ? '#'+str.split(' ').map(st=> st.charAt(0).toUpperCase()+st.slice(1)).join('') 
+  : false
 }
